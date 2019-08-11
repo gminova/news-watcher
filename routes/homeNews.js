a6a2ff91a39a9c0b8c7951f5e94b1840004982e0
gminova <minova1995@gmail.com>
2019-08-11 11:33:30 +0100

    homeNews function to get array of news stories

--- routes/homeNews.js
+++ routes/homeNews.js
@@ -3,4 +3,17 @@
 let express = require("express");
 let router = express.Router();
 
+//Retrieve all top news, same for all users
+router.get("/", function(req, res, next) {
+  req.db.collection.findOne(
+    { _id: process.env.GLOBAL_STORIES_ID },
+    { homeNewsStories: 1 },
+    function(err, doc) {
+      if (err) return next(err);
+
+      res.status(200).json(doc.homeNewsStories);
+    }
+  );
+});
+
 module.exports = router;

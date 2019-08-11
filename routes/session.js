24310e66a7b4f3d217dbfad47cb97e46853e9504
gminova <minova1995@gmail.com>
2019-08-11 12:16:48 +0100

    typo

--- routes/session.js
+++ routes/session.js
@@ -1,7 +1,7 @@
 //A module for session login and logout
 "use strict";
 const express = require("express");
-const bcrypt = require("bycriptjs"); //for password hash comparing
+const bcrypt = require("bcryptjs"); //for password hash comparing
 const jwt = require("jwt-simple"); //for token authentication
 const joi = require("joi"); //for data validation
 const authHelper = require("./authHelper");
